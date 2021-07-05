b832ab7b923d01cacf717bd6fbc04fa3de9541db
phisham <84333011+phisham@users.noreply.github.com>
2021-07-04 23:38:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,11 @@ app.post("/",function(req,res){
        if(err) throw err;
        var a=data.search(n1);
        var b=data.search(n2);
-       if(a!=-1 || b!=-1){
-           res.send("This username or password exist !!!please change the username/password.");
+       if(n1=='' || n2==''){
+           res.send("Enter username/password");
+       }
+       else if(a!=-1 || b!=-1){
+           res.send("This username/password exist !!!please change the username/password.");
        }
        else {
             var fileuser=fs.appendFile('namereg.txt',n+' ',function(err){
@@ -45,7 +48,7 @@ app.post("/done",function(req,res){
       var fr=fs.readFile('namereg.txt','utf-8',function(err,data){
           if(err) throw err;
           var h=data.search(read_n+' ');
-          if(h==-1){
+          if(h==-1 || n3=='' || n4==''){
             res.send("Login Failed!!");
           }
           else res.send("Login Successful!!!")
@@ -58,4 +61,3 @@ app.post("/done",function(req,res){
 app.listen(3600,function(){
     console.log("Server has started on port 3600");
 });
-
